ab69a0aa385d229a95de2dc210a83f81aeb23e18
Khaled Sourani <khaledsourani98@gmail.com>
2023-08-27 19:15:58 +0300

    added units

---
+++ app/Http/Controllers/MeasuringUnitController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\MeasuringUnit;
+use App\Http\Requests\StoreMeasuringUnitRequest;
+use App\Http\Requests\UpdateMeasuringUnitRequest;
+
+class MeasuringUnitController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreMeasuringUnitRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(MeasuringUnit $measuringUnit)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(MeasuringUnit $measuringUnit)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateMeasuringUnitRequest $request, MeasuringUnit $measuringUnit)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(MeasuringUnit $measuringUnit)
+    {
+        //
+    }
+}
---
+++ database/migrations/2023_08_27_160953_create_measuring_units_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('measuring_units', function (Blueprint $table) {
+            $table->id();
+            $table->string('title',80);
+            $table->unsignedTinyInteger('type')->comment('1 lab,2 phar');
+            $table->unsignedTinyInteger('medicine_type')->nullable()->comment('1 purch, 2 trade');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+
+
+
+
+
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->foreign('deleted_by')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('measuring_units');
+    }
+};
