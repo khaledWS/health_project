3fa8e844d4cd94cb088c103ec1dfa03d14f9d409
Khaled Sourani <khaledsourani98@gmail.com>
2023-08-28 07:31:46 +0300

    added profile tests files

---
+++ database/migrations/2023_08_28_043052_create_profile_tests_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profile_tests', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedMediumInteger('category_id');
+            $table->string('title',100);
+            $table->string('test_code',30);
+            $table->string('description',250);
+            $table->string('info',250);
+            $table->string('globalCode',50);
+            $table->string('testcode',50);
+            $table->unsignedTinyInteger('split');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+            $table->unsignedBigInteger('deleted_by')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+
+            $table->foreign('created_by')->references('id')->on('users');
+            $table->foreign('updated_by')->references('id')->on('users');
+            $table->foreign('deleted_by')->references('id')->on('users');
+            $table->foreign('category_id')->references('id')->on('test_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profile_tests');
+    }
+};
---
+++ database/seeders/ProfileTestSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ProfileTestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
