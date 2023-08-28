ce8a7215ff8022c7e433c431f0037aa54bf4e6c7
Khaled Sourani <khaledsourani98@gmail.com>
2023-08-28 07:05:51 +0300

    machines

---
+++ app/Http/Controllers/MachinesController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\machines;
+use App\Http\Requests\StoremachinesRequest;
+use App\Http\Requests\UpdatemachinesRequest;
+
+class MachinesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoremachinesRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(machines $machines)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(machines $machines)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdatemachinesRequest $request, machines $machines)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(machines $machines)
+    {
+        //
+    }
+}
