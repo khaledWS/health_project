7945a65edee9c44106af890cce21d7aee091cdcf
Khaled Sourani <khaledsourani98@gmail.com>
2023-08-28 07:03:00 +0300

    added lab specimen

---
+++ app/Http/Controllers/LabSpecimenController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\LabSpecimen;
+use App\Http\Requests\StoreLabSpecimenRequest;
+use App\Http\Requests\UpdateLabSpecimenRequest;
+
+class LabSpecimenController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreLabSpecimenRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(LabSpecimen $labSpecimen)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(LabSpecimen $labSpecimen)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateLabSpecimenRequest $request, LabSpecimen $labSpecimen)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(LabSpecimen $labSpecimen)
+    {
+        //
+    }
+}
