db0d5cefa1167f583dc97923fc86766e4901b59d
Khaled Sourani <khaledsourani98@gmail.com>
2023-08-29 18:35:01 +0300

    added database

--- routes/api.php
+++ routes/api.php
@@ -31,7 +31,7 @@ Route::post('/login', function (Request $request) {
 
 Route::post('/get-data-from-mio/forwardtelemetry', function (Request $request){
   // return $request->deviceId;
-  DB::table('test_mio')->insert([
+  DB::connection('mysql2')->table('test_mio')->insert([
     'data' => json_encode($request->all())
   ]);
 });
